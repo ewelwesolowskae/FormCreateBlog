9b557e969f1d1e763e48afec6f3cdfb419b5acc2
PSulewski <zalesiak1209@wp.pl>
2021-03-05 10:43:13 +0100

    dodanie bazy danych, rozszerzenie modelu uzytkownika

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'apps_static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'public/uploads/'
+AUTH_USER_MODEL = 'accounts.CustomUser'
+try:
+    # Project
+    from project.settings_local import *  # noqa: F403, F401
+except ImportError:
+    pass
